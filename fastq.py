07c4a5a6b3d5ca140ace87ddc6e93f2fddb44511
Alisa Fedorenko <afedorenko00@gmail.com>
2023-10-05 13:16:31 +0300

    add gc_content function

---
+++ fastq.py
@@ -0,0 +1,15 @@
+#GC content frequency function
+
+def gc_content(sequence: str) -> float:
+    """
+    Calculates the GC content in a fastq sequence.
+
+    :param sequence: fastq sequence
+    :type sequence: str
+    :return: GC content as a percentage
+    :rtype: float
+    """
+    gc_count = sequence.count('G') + sequence.count('C')
+    total_count = len(sequence)
+    gc_content = round((gc_count / total_count) * 100, 2)
+    return gc_content
