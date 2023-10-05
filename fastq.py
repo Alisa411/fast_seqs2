1531d328c77aef633d771d51f54695f20d99de57
Alisa Fedorenko <afedorenko00@gmail.com>
2023-10-05 23:39:59 +0300

    add mean_encoding_offset function

--- fastq.py
+++ fastq.py
@@ -28,5 +28,24 @@ def seq_length(sequence: str) -> int:
     total_count = len(sequence)
     return total_count
 
+#the mean encoding offset for quality scores function
+def mean_encoding_offset(quality_string):
+    """
+    Calculate the mean encoding offset for quality scores in the input string.
+
+    :param quality_string: A string of quality scores
+    :type quality_string: str
+    :return: The mean encoding offset
+    :rtype: float
+    """
+    total_offset = 0
+    for char in quality_string:
+        offset = ord(char) - 33  # Assuming 33 as the default encoding offset
+        total_offset += offset
+    mean_offset = total_offset / len(quality_string)
+    return mean_offset
+
+
+
 
 
