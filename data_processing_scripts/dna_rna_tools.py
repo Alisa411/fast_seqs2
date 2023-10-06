968cd1ec114755a4d22a068f0a494d92761663e7
Alisa Fedorenko <afedorenko00@gmail.com>
2023-10-06 23:54:58 +0300

    fix bags in transcribe, complement, reverse_complement functions and for cycle

---
+++ data_processing_scripts/dna_rna_tools.py
@@ -0,0 +1,43 @@
+TRANSCRIBE_DICT = {'T': 'U', 't': 'u'}
+COMPLEMENT_DICT = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'a': 't', 't': 'a', 'c': 'g', 'g': 'c'}
+
+
+def transcribe(sequence):
+    return ''.join(TRANSCRIBE_DICT[base] if base in TRANSCRIBE_DICT else base for base in sequence)
+
+
+def reverse(sequence):
+    return sequence[::-1]
+
+
+def complement(sequence):
+    return ''.join(COMPLEMENT_DICT[base] if base in COMPLEMENT_DICT else base for base in sequence)
+
+
+def reverse_complement(sequence):
+    return reverse(complement(sequence))
+
+
+def run_dna_rna_tools(*argument):
+    action = argument[-1].lower()
+    sequences = argument[:-1]
+    results = []
+
+    for sequence in sequences:
+        if all(base in 'ATCGUatcgu' for base in sequence):
+            if action == 'transcribe':
+                result = transcribe(sequence)
+            elif action == 'reverse':
+                result = reverse(sequence)
+            elif action == 'complement':
+                result = complement(sequence)
+            elif action == 'reverse_complement':
+                result = reverse_complement(sequence)
+        else:
+            result = f"Invalid procedure: {action}"
+    else:
+        result = "Invalid dna/rna sequence"
+
+        results.append(result)
+
+        return results if len(results) > 1 else results[0]
