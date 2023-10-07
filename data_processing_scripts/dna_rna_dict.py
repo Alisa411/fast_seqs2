c3e3327956bc347a8e36bd416b65ad4e2d3995d4
Alisa Fedorenko <afedorenko00@gmail.com>
2023-10-07 14:59:37 +0300

    add dictionaries for transcribe and complement functions

--- data_processing_scripts/dna_rna_dict.py
+++ data_processing_scripts/dna_rna_dict.py
@@ -1,2 +1,20 @@
 #rna_dna_dict
 
+# Dictionary: keys - nucleotide letter of dna; values - complement nucleotide letter of rna
+TRANSCRIBE_DICT = {
+    'T': 'U',
+    't': 'u'
+}
+
+# Dictionary: keys - nucleotide letter of dna; values - complement nucleotide letter of dna
+COMPLEMENT_DICT = {
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C',
+    'a': 't',
+    't': 'a',
+    'c': 'g',
+    'g': 'c'}
+
+
