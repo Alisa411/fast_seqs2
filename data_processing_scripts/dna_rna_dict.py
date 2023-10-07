52422637152e61b951d7d77dad3fbe0b4028501a
Alisa Fedorenko <afedorenko00@gmail.com>
2023-10-07 15:25:29 +0300

    add dictionaries for dna and rna letters

--- data_processing_scripts/dna_rna_dict.py
+++ data_processing_scripts/dna_rna_dict.py
@@ -1,10 +1,17 @@
-#rna_dna_dict
+# DNA set of letters
+DNA_LETTERS = set("ATGCatgc")
+
+
+# RNA set of letters
+RNA_LETTERS = set("AUGCaugc")
+
 
 # Dictionary: keys - nucleotide letter of dna; values - complement nucleotide letter of rna
 TRANSCRIBE_DICT = {
     'T': 'U',
     't': 'u'
 }
+
 
 # Dictionary: keys - nucleotide letter of dna; values - complement nucleotide letter of dna
 COMPLEMENT_DICT = {
@@ -16,5 +23,3 @@ COMPLEMENT_DICT = {
     't': 'a',
     'c': 'g',
     'g': 'c'}
-
-
